489edb6803eaede10709e3022ca4d2c8b1adca83
J200W <94389835+J200W@users.noreply.github.com>
2024-07-30 16:01:31 +0200

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "efresto";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
